6f6812dd20b943fdf60b676219aacd4ca7ec3f7c
Nishant Tomar <nishanttomar21@gmail.com>
2021-01-13 03:03:31 +0530

    encryption fully done

--- Hill-Cipher.py
+++ Hill-Cipher.py
@@ -10,7 +10,8 @@ import re
 
 # Key (3x3 matrix therefore, plaintext matrix possible matrices - 3x1, 3x2, 3x3)
 key_matrix = [[6, 5, 1], [3, 2, 3], [4, 3, 2]]
-rows = columns = 3                                                                                                                  # Column and row size of matrix
+rows = 3                                                                                                                        # Row size of plain-text message
+column = 0                                                                                                                      # Column size of plain-text message
 
 
 # To get integer associated with that character
@@ -41,7 +42,7 @@ def check_length(input):
 
 # Check if any illegal characters present in the string
 def check_illegal_char(input):
-    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')                                                                                 # Regex
+    regex = re.compile("[@_!#$%^&*()<>?/\|}{~:]")                                                                                 # Regex
 
     return True if regex.search(input) == None else False
 
@@ -55,6 +56,25 @@ def check_input(input):
     return False
 
 
+# To convert the given string into matrix form (3 x X) - 3x1, 3x2, 3x3 (Depending of the input length)
+def convert_string_to_matrix(string):
+    # Initializing the variables
+    row = column = 0
+    matrix = [[0 for i in range(columns)] for j in range(rows)]
+
+    # Iterating to fill the matrix
+    for index, value in enumerate(string):
+        matrix[row][column] = value
+        column += 1                                                                                                             # Incrementing column value
+
+        # Condition to change the indexes
+        if ((index+1) % columns) == 0:
+            row = int((index+1) / columns)
+            column = 0
+
+    return matrix
+
+
 # To take plain-text input from the user (Message to be encrypted)
 def take_input():
     print("\n\n####################################")
@@ -64,7 +84,7 @@ def take_input():
 
     # Loop till the input is not valid
     while True:
-        input_string = input("Enter your message to be encrypted in capital-letters (Input length of message should be 3, 6 or 9): ")
+        input_string = input("Enter your message to be encrypted in capital-letters (Input length of message should be 3, 6 or 9): ").upper()
         flag = check_input(input_string)
 
         if flag:
@@ -72,26 +92,17 @@ def take_input():
         else:
             print("\nPlease try again (Invalid input)!")
 
+    # Setting up the number of columns globally
+    global columns
+    columns = int(len(input_string)/3)
 
-'''
-    # Taking matrix input in one line
-    character_input_matrix = [[input("Input[" + str(y+1) + "][" + str(x+1) + "]: ").upper() for x in range(columns)] for y in range(rows)]
+    return input_string
 
-    # Printing the input matrix
-    print("\n\nEntered input matrix")
-    [[print(character_input_matrix[y][x] + " ", end = "\n" if x in (2, 5, 8) else " ") for x in range(columns)] for y in range(rows)]
-    print("\n\n")
-
-    # Convertor character matrix to integer matrix
-    integer_input_matrix = [[char_to_int(character_input_matrix[y][x]) for x in range(columns)] for y in range(rows)]
-
-    return integer_input_matrix
-'''
 
 # Function to perform matrix multiplication between 2 matrices
 def matrix_multiplication(matrix1, matrix2):
-    # Resultant matrix
-    result_matrix = [[0, 0, 0] for i in range(rows)]
+    # Resultant matrix (Initialization)
+    result_matrix = [[0 for i in range(columns)] for j in range(rows)]
 
     # Iterating by row of matrix1
     for i in range(len(matrix1)):
@@ -111,8 +122,10 @@ def display_encrypted_message(matrix):
     [[print(int_to_char(int(matrix[x][y])), end = " ") for y in range(columns)] for x in range(rows)]
 
 # Function to encrypt the message
-def encryption(matrix):
-    encrypted_matrix = matrix_multiplication(key_matrix, matrix)                                                                  # Matrix multiplication ( C = K * P mod 26)
+def encryption(input_matrix):
+    integer_message_matrix = [[char_to_int(input_matrix[y][x]) for x in range(columns)] for y in range(rows)]
+    encrypted_matrix = matrix_multiplication(key_matrix, integer_message_matrix)                                                  # Matrix multiplication ( C = K * P mod 26)
+
     display_encrypted_message(encrypted_matrix)
 
     return encrypted_matrix
@@ -120,6 +133,6 @@ def encryption(matrix):
 
 # So that this function doesn't run when some other file imports this file
 if __name__ == "__main__":
-    take_input()
-    #message_matrix = take_input()
-    #encrypted_matrix = encryption(message_matrix)
+    message_string = take_input()
+    character_message_matrix = convert_string_to_matrix(message_string)
+    integer_encrypted_matrix = encryption(character_message_matrix)
